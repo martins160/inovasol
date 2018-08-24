f47278dc2f5895ff04f0935809ed83e38a12b6b4
Marcelo Martins <marcelo@credvalor.local>
2018-08-24 13:24:22 -0300

    Modelo Inova Sol Pronto

---
+++ #####index.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Inovasol</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.3/cerulean/bootstrap.min.css" />	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header class="header">
+
+<div class="header-top shadow">
+	<span class="header-tel"><a href="tel:3434239298"><i class="fa fa-phone"></i> (34) 3423-9298</a></span>
+	<span class="header-face"><a href="" target="_blank"><i class="fa fa-facebook"></i></a> /inovasol</span>
+	<span class="header-mail"><a href="mailto:atendimento@inovasol-mg.com.br"><i class="fa fa-envelope"></i></a> atendimento@inovasol-mg.com.br</span>
+</div>
+
+
+<div class="header-bloco2">
+	<div class="container">
+		<div class="row">
+			<div class="col">	
+				<div class="logo">			
+					<img src="assets/images/logo/01.png" class="img-fluid">
+					<a href="" class="btn-simula-topo"><span>$</span> Simule sua economia</a>
+					<a class="ssm-toggle-nav nav-flex" href="#" title="Open / close">MENU<i class="fa fa-bars" aria-hidden="true"></i> </a>
+					<nav>
+						<div class="nav-logo">
+							<img src="assets/images/logo/01.png" class="img-fluid">
+						</div>
+						<div class="nav-menu-flex">
+							<a href="">MENU 1</a>
+							<a href="">MENU 2</a>
+							<a href="">MENU 3</a>
+							<a href="">MENU 4</a>
+						</div>	
+						<div class="nav-exit">					
+							<a href="#" class="ssm-toggle-nav"><i class="fa fa-times" aria-hidden="true"></i> Fechar</a>
+						</div>			
+					</nav>					
+				</div>
+			</div>
+		</div>
+		<div class="nav-top2">
+			<a href="" class="btn-simula-topo2"><span>$</span> Simule sua economia</a>
+		</div>
+	</div>	
+</div>	
+
+<div class="header-bg">
+	<img src="assets/images/backgrounds/bg03.jpg">
+</div>
+
+<div class="header-txt">
+	<div class="header-txt-bloc">
+		Reduza em <span class="header-pp">até 98%</span> sua conta de Energia		
+	</div>
+</div>
+
+<div class="header-icon">	
+	<div class="arrow"><i class="fa fa-angle-down" aria-hidden="true"></i></div>		
+</div>
+
+<div class="slider">
+	<div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
+	  <ol class="carousel-indicators" style="z-index: 1">
+	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img class="d-block w-100" src="assets/tmp/slider/bg01.png" alt="First slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="assets/tmp/slider/bg02.jpg" alt="First slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="assets/tmp/slider/bg03.jpg" alt="First slide">
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev" style="z-index: 2">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next" style="z-index: 2">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+</div>
+
+</header>
+
+<main>
+
+	<div class="content-func">
+		<span class="func-titu">VEJA COMO FUNCIONA</span>
+		<span class="func-sub-titu">Temos a melhor solução em energia limpa,
+	sustentável e econômica, para você ou seu negócio</span>
+	</div>
+
+	<div class="content-ilustra">
+
+		<div class="content-ilustra-01">
+			<img src="assets/images/backgrounds/bg04.png">
+		</div>		
+
+		<div id="carouselIlustra" class="carousel slide carousel-fade" data-ride="carousel">
+		  <!--<ol class="carousel-indicators" style="z-index: 1">
+		    <li data-target="#carouselIlustra" data-slide-to="0" class="active"></li>
+		    <li data-target="#carouselIlustra" data-slide-to="1"></li>
+		  </ol>-->
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img class="d-block w-100" src="assets/tmp/slider/bg04.png" alt="First slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="assets/tmp/slider/bg05.png" alt="First slide">
+		    </div>
+		  </div>
+		  <a class="carousel-control-prev" href="#carouselIlustra" role="button" data-slide="prev" style="z-index: 2">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="carousel-control-next" href="#carouselIlustra" role="button" data-slide="next" style="z-index: 2">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>		
+	</div>
+
+	<div class="beneficios">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="beneficios-tit">Alguns benefícios</div>
+					<div class="beneficios-icon"></div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 beneficios-flex">
+
+					<div class="beneficio-bloc">
+						<i class="fa fa-battery-full"></i>
+						<span class="beneficio-bloc-titu">Economia</span>
+						<span class="beneficio-bloc-txt">Reduza até 98% da sua fatura</span>						
+					</div>					
+					<div class="beneficio-bloc">
+						<i class="fa fa-leaf"></i>
+						<span class="beneficio-bloc-titu">Sustentável</span>
+						<span class="beneficio-bloc-txt">Gere sua própria energia limpa e renovável</span>						
+					</div>					
+					<div class="beneficio-bloc">
+						<i class="fa fa-percent"></i>
+						<span class="beneficio-bloc-titu">Retorno</span>
+						<span class="beneficio-bloc-txt">Economia imediata na conta de luz sobre o invertimento</span>						
+					</div>					
+					<div class="beneficio-bloc">
+						<i class="fa fa-usd"></i>
+						<span class="beneficio-bloc-titu">Valorização</span>
+						<span class="beneficio-bloc-txt">O sistema valoriza seu imóvel em cerca de 8%</span>						
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="projetos">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="projetos-tit">Projetos INOVA <span class="projetos-sol">SOL</span></div>
+					<div class="projetos-icon"></div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 projetos-flex">
+
+					<div class="projetos-bloc">	
+						<div class="projetos-container">
+							<a href="">
+							  <img src="assets/tmp/projetos/01.jpg" class="img-fluid">
+							  <div class="overlay">
+							    <div class="icon">
+							      <i class="fa fa-search" aria-hidden="true"></i> 
+							    </div>
+							  </div>
+						  </a>
+						</div>
+						<span class="projetos-potencia">Potência: 2,5kWp</span>
+						<span class="projetos-tipo"><strong>Tipo:</strong> Empresa</span>
+						<span class="projetos-local"><strong>Local:</strong> Frutal</span>
+					</div>					
+					<div class="projetos-bloc">
+						<div class="projetos-container">
+							<a href="">
+							  <img src="assets/tmp/projetos/02.jpg" class="img-fluid">
+							  <div class="overlay">
+							    <div class="icon">
+							      <i class="fa fa-search" aria-hidden="true"></i> 
+							    </div>
+							  </div>
+						  </a>
+						</div>						
+						<span class="projetos-potencia">Potência: 2,6kWp</span>
+						<span class="projetos-tipo"><strong>Tipo:</strong> Residência</span>
+						<span class="projetos-local"><strong>Local:</strong> Frutal</span>
+					</div>					
+					<div class="projetos-bloc">
+						<div class="projetos-container">
+							<a href="">
+							  <img src="assets/tmp/projetos/03.jpg" class="img-fluid">
+							  <div class="overlay">
+							    <div class="icon">
+							      <i class="fa fa-search" aria-hidden="true"></i> 
+							    </div>
+							  </div>
+						  </a>
+						</div>							
+						<span class="projetos-potencia">Potência: 2,5kWp</span>
+						<span class="projetos-tipo"><strong>Tipo:</strong> Residência</span>
+						<span class="projetos-local"><strong>Local:</strong> Uberlândia</span>
+					</div>					
+					<div class="projetos-bloc">
+						<div class="projetos-container">
+							<a href="">
+							  <img src="assets/tmp/projetos/04.jpg" class="img-fluid">
+							  <div class="overlay">
+							    <div class="icon">
+							      <i class="fa fa-search" aria-hidden="true"></i> 
+							    </div>
+							  </div>
+						  </a>
+						</div>							
+						<span class="projetos-potencia">Potência: 2,1kWp</span>
+						<span class="projetos-tipo"><strong>Tipo:</strong> Rural</span>
+						<span class="projetos-local"><strong>Local:</strong> Frutal</span>
+					</div>					
+					<div class="projetos-bloc">
+						<div class="projetos-container">
+							<a href="">
+							  <img src="assets/tmp/projetos/05.jpg" class="img-fluid">
+							  <div class="overlay">
+							    <div class="icon">
+							      <i class="fa fa-search" aria-hidden="true"></i> 
+							    </div>
+							  </div>
+						  </a>
+						</div>							
+						<span class="projetos-potencia">Potência: 3,2kWp</span>
+						<span class="projetos-tipo"><strong>Tipo:</strong> Empresa</span>
+						<span class="projetos-local"><strong>Local:</strong> Frutal</span>
+					</div>					
+					<div class="projetos-bloc">
+						<div class="projetos-container">
+							<a href="">
+							  <img src="assets/tmp/projetos/06.jpg" class="img-fluid">
+							  <div class="overlay">
+							    <div class="icon">
+							      <i class="fa fa-search" aria-hidden="true"></i> 
+							    </div>
+							  </div>
+						  </a>
+						</div>							
+						<span class="projetos-potencia">Potência: 1,3kWp</span>
+						<span class="projetos-tipo"><strong>Tipo:</strong> Residência</span>
+						<span class="projetos-local"><strong>Local:</strong> Frutal</span>
+					</div>
+
+				</div>
+				<a href="" class="btn-veja-mais"><i class="fa fa-plus" aria-hidden="true"></i> Veja Mais</a>
+			</div>
+		</div>
+	</div>
+
+</main>
+
+
+<footer class="footer">
+	<div class="footer-titu">Entre em contato conosco!</div>
+	<div class="footer-icon"></div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 footer-flex">
+				<div class="footer-blocos">
+					<div class="footer-blocos-linha"><i class="fa fa-home" aria-hidden="true"></i></div>
+					<div class="footer-blocos-linha footer-blocos-end">Endereço</div>
+					<div class="footer-blocos-linha footer-blocos-r">Frutal - MG<br>Av. Rio de Janeiro, 450 Centro, 31080-400</div>
+				</div>
+				<div class="footer-blocos">
+					<div class="footer-blocos-linha"><i class="fa fa-mobile" aria-hidden="true"></i></div>
+					<div class="footer-blocos-linha footer-blocos-tel">Tel/E-mail</div>
+					<div class="footer-blocos-linha footer-blocos-r"><span class="fbt">(34) 3423-9298</span><br>contato@inovasol-mg.com.br</div>
+				</div>
+				<div class="footer-blocos">
+					<div class="footer-blocos-linha"><i class="fa fa-question-circle" aria-hidden="true"></i></div>
+					<a href=""><div class="footer-blocos-linha footer-blocos-questions">Clique aqui e tire suas dúvidas</div></a>
+				</div>								
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 footer-flex-rod">
+				<div class="footer-blocos-rod">
+					<div class="footer-blocos-linha footer-blocos-siga">Siga a Inova Sol</div>
+					<div class="footer-blocos-linha footer-blocos-social">Acompanhe nossas novidades nas redes sociais</div>
+					<div class="footer-blocos-linha footer-blocos-icon"><a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a></div>
+					<div class="footer-blocos-linha"><img src="assets/images/logo/01.png" alt=""></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<script src="http://labs.rampinteractive.co.uk/touchSwipe/jquery.touchSwipe.min.js"></script>
+<script src="http://joanclaret.github.io/slide-and-swipe-menu/jquery.slideandswipe.js"></script>
+
+<script>
+	//SLIDE MENU
+    $(document).ready(function() {
+        $('nav').slideAndSwipe();
+    });
+
+
+    $('#carouselIlustra').carousel({
+	  interval: 10000,
+	  pause: 'hover'
+	})
+</script>
+
+</body>
+</html>
